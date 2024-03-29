d327c42719ce0d44fc42e71a4a580a7e148be820
RobertoDPH <robertodelpino11@gmail.com>
2024-03-29 13:31:22 +0000

    Added chat group view to chat list

--- src/pages/Chat/components/ChatList/ChatList.jsx
+++ src/pages/Chat/components/ChatList/ChatList.jsx
@@ -43,55 +43,106 @@ const ChatList = () => {
                         ? <div className="text-center" >Comience una nueva conversacion</div> 
                         : (
                             sortedChats.map((chat) => {
-                                return chat.users.map( (user, index) => {
+                                return chat.isGroup ? (
+                                    <>
+                                        <article 
+                                            className={"w-11/12 my-1 mx-auto p-2 rounded-md flex hover:cursor-pointer hover:bg-highlight " + (selectedChat._id == chat._id ? "bg-highlight" : "")} 
+                                            onClick={() => handleSelectChat(chat)}
+                                            key={chat._id}>
+                                            <div className="w-[85%] flex items-center">
+                                                <div className="w-10 h-10 min-w-[40px] flex items-center">
+                                                    <UserPhoto getProfilePhoto={getProfilePhoto} user={chat.users[0]}/>
+                                                </div>
+                                                <div className="w-[90%] overflow-hidden">
+                                                    <p className="w-full mx-3 font-bold text-lg whitespace-nowrap overflow-hidden">{chat.groupName} </p>
+                                                    {
+                                                        chat.messages.length !== 0 
+                                                            ? <div className="w-full mx-3">
+                                                                <section className="flex justify-between">
+                                                                    {
+                                                                        chat.messages.length !== 0 && chat.messages[0].sender == auth._id
+                                                                            ? <p className="w-8 whitespace-nowrap">
+                                                                                {chat.messages[0].readed
+                                                                                    ? <img src={DoubleCheckBlue} alt="" /> 
+                                                                                    : <img src={DoubleCheckGray} alt="" /> }
+                                                                            </p>
+                                                                            : ""
+                                                                    }
+                                                                    <p className="w-full inline-block whitespace-nowrap overflow-hidden">
+                                                                        {chat.messages.length !== 0 && chat.messages[0].message }
+                                                                    </p>
+                                                                </section>
+                                                            </div>
+                                                            : <p className="mx-3">Comienza enviando un mensaje</p>
+                                                    }
+                                                    
+                                                </div>
+                                            </div>
+
+                                            <div className="w-[15%] text-center">
+                                                <p>
+                                                    {chat.messages.length !== 0 && new Date(chat.messages[0].createdAt).toLocaleTimeString("es-ES", {hour: '2-digit', minute:'2-digit'})}
+                                                </p>
+                                                {
+                                                    (chat.messages.length !== 0 && chatsWithNewMessages.includes(chat._id) || 
+                                                        (chat.messages.length !== 0 && !chat.messages[0].readed && chat.messages[0].sender != auth._id)) && (
+                                                        <div className="flex justify-center">
+                                                            <div className="w-5 h-5 rounded-full bg-red-500"></div>
+                                                        </div>
+                                                    )
+                                                }
+                                            </div>
+                                            
+                                        </article>
+                                    </>
+                                )
+                                : chat.users.map( (user, index) => {
                                     if(user._id != auth._id){
-                                        return chat.messages.length == 0
-                                            ? "" 
-                                            :   <article 
-                                                    className={"w-11/12 my-1 mx-auto p-2 rounded-md flex hover:cursor-pointer hover:bg-highlight " + (selectedChat._id == chat._id ? "bg-highlight" : "")} 
-                                                    onClick={() => handleSelectChat(chat)}
-                                                    key={index}>
-                                                        <div className="w-[85%] flex items-center">
-                                                            <div className="w-10 h-10 min-w-[40px] flex items-center">
-                                                                <UserPhoto getProfilePhoto={getProfilePhoto} user={user}/>
-                                                            </div>
-                                                            <div className="w-[90%] overflow-hidden">
-                                                                <p className="w-full mx-3 font-bold text-lg whitespace-nowrap overflow-hidden">{user.name} </p>
-                                                                <div className="w-full mx-3">
-                                                                    <section className="flex justify-between">
-                                                                        {
-                                                                            chat.messages[0].sender == auth._id
-                                                                                ? <p className="w-8 whitespace-nowrap">
-                                                                                    {chat.messages[0].readed
-                                                                                        ? <img src={DoubleCheckBlue} alt="" /> 
-                                                                                        : <img src={DoubleCheckGray} alt="" /> }
-                                                                                </p>
-                                                                                : ""
-                                                                        }
-                                                                        <p className="w-full inline-block whitespace-nowrap overflow-hidden">
-                                                                            {chat.messages[0].message }
+                                        return <article 
+                                            className={"w-11/12 my-1 mx-auto p-2 rounded-md flex hover:cursor-pointer hover:bg-highlight " + (selectedChat._id == chat._id ? "bg-highlight" : "")} 
+                                            onClick={() => handleSelectChat(chat)}
+                                            key={index}>
+                                                <div className="w-[85%] flex items-center">
+                                                    <div className="w-10 h-10 min-w-[40px] flex items-center">
+                                                        <UserPhoto getProfilePhoto={getProfilePhoto} user={user}/>
+                                                    </div>
+                                                    <div className="w-[90%] overflow-hidden">
+                                                        <p className="w-full mx-3 font-bold text-lg whitespace-nowrap overflow-hidden">{user.name} </p>
+                                                        <div className="w-full mx-3">
+                                                            <section className="flex justify-between">
+                                                                {
+                                                                    chat.messages[0].sender == auth._id
+                                                                        ? <p className="w-8 whitespace-nowrap">
+                                                                            {chat.messages[0].readed
+                                                                                ? <img src={DoubleCheckBlue} alt="" /> 
+                                                                                : <img src={DoubleCheckGray} alt="" /> }
                                                                         </p>
-                                                                    </section>
-                                                                </div>
+                                                                        : ""
+                                                                }
+                                                                <p className="w-full inline-block whitespace-nowrap overflow-hidden">
+                                                                    {chat.messages[0].message }
+                                                                </p>
+                                                            </section>
+                                                        </div>
+                                                    </div>
+                                                </div>
+
+                                                <div className="w-[15%] text-center">
+                                                    <p>
+                                                        {new Date(chat.messages[0].createdAt).toLocaleTimeString("es-ES", {hour: '2-digit', minute:'2-digit'})}
+                                                    </p>
+                                                    {
+                                                        (chatsWithNewMessages.includes(chat._id) || 
+                                                            (!chat.messages[0].readed && chat.messages[0].sender != auth._id)) && (
+                                                            <div className="flex justify-center">
+                                                                <div className="w-5 h-5 rounded-full bg-red-500"></div>
                                                             </div>
-                                                        </div>
 
-                                                        <div className="w-[15%] text-center">
-                                                            <p>
-                                                                {new Date(chat.messages[0].createdAt).toLocaleTimeString("es-ES", {hour: '2-digit', minute:'2-digit'})}
-                                                            </p>
-                                                            {
-                                                                (chatsWithNewMessages.includes(chat._id) || 
-                                                                    (!chat.messages[0].readed && chat.messages[0].sender != auth._id)) && (
-                                                                    <div className="flex justify-center">
-                                                                        <div className="w-5 h-5 rounded-full bg-red-500"></div>
-                                                                    </div>
-
-                                                                )
-                                                            }
-                                                        </div>
-                                                        
-                                                </article>
+                                                        )
+                                                    }
+                                                </div>
+                                                
+                                        </article>
                                     }
                                 })
                             })
