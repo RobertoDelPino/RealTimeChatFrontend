a03bdcf3781f2814130c1b2992e6e5845c725112
RobertoDPH <robertodelpino11@gmail.com>
2024-04-09 12:58:08 +0100

    simplify notification

--- src/helpers/notify.jsx
+++ src/helpers/notify.jsx
@@ -1,11 +1,5 @@
 import toast from "react-hot-toast";
 
 export const notify = (message, type) => {
-    if(type === 'success') {
-        return toast.success(message)
-    }
-
-    if(type === 'error') {
-        return toast.error(message)
-    }
+    toast[type](message);
 };
